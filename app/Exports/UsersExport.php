9214b0ccd9ba88afa0382272c9a4b2fb1c52286c
muhohoweb@gmail.com <muhohoweb@gmail.com>
2022-09-25 21:51:53 +0300

    Post to slack

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\FromCollection;
@@ -31,6 +32,17 @@ class UsersExport implements FromArray
                 $file['values']['documentsize'],
             ]);
         }
+
+        $response = Http::post('http://example.com/users', [
+            'name' => 'Steve',
+            'role' => 'Network Administrator',
+        ]);
+
+        $slackUrl = "https://hooks.slack.com/services/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";
+
+        $response = Http::acceptJson()->post($slackUrl,[
+            'text' => "Hello from Docker",
+        ]);
 
         return [
             ["File Name", "Created", "Modified",'Size'],
