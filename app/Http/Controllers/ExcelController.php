c5b70ca2c20805057add34a50e62e62a6efe616e
muhohoweb@gmail.com <muhohoweb@gmail.com>
2022-09-29 02:22:41 +0300

    date

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Exports\UsersExport;
+use App\Mail\FilesTransfered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ExcelController extends Controller
@@ -14,6 +16,7 @@ class ExcelController extends Controller
     {
         $files = $request->get('data');
         $fileName = now()->format('d-m-Y')."-".'transferred-files.xlsx';
-        return Excel::store(new UsersExport($files), $fileName);
+        Excel::store(new UsersExport($files), $fileName);
+       return Mail::to('jeremiah.muhoho@thejitu.com')->send(new FilesTransfered($data));
     }
 }
