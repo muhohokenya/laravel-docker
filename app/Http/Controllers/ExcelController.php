fb8de5be1ea67fdf926cb1cd88f09a0ed1d813a4
muhohoweb@gmail.com <muhohoweb@gmail.com>
2022-09-29 02:11:20 +0300

    date

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -13,6 +13,6 @@ class ExcelController extends Controller
     public function export(Request $request)
     {
         $files = $request->get('data');
-        return Excel::store(new UsersExport($files), now()->format('d-m-y').'files.xlsx');
+        return Excel::store(new UsersExport($files), now()->format('d-m-D')."-".'files.xlsx');
     }
 }
